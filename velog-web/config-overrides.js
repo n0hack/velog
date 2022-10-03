86a6d57ad9287a6d15f64f70764d0c1227447aab
n0hack <nohack-@naver.com>
2022-10-04 06:27:28 +0900

    [chore] 절대경로 추가 (@containers)

--- velog-web/config-overrides.js
+++ velog-web/config-overrides.js
@@ -6,7 +6,9 @@ module.exports = override(
   useBabelRc(),
   addWebpackAlias({
     '@components': path.resolve(__dirname, 'src/components'),
+    '@containers': path.resolve(__dirname, 'src/containers'),
     '@lib': path.resolve(__dirname, 'src/lib'),
+    '@modules': path.resolve(__dirname, 'src/modules'),
     '@pages': path.resolve(__dirname, 'src/pages'),
   }),
 );
