617b7fdab549acefe0f60d61ea06eff02ee7f9ab
n0hack <nohack-@naver.com>
2022-11-04 14:37:35 +0900

    [fix] Button 컴포넌트 접근성 고려해 재설계

--- frontend/src/components/common/Button.tsx
+++ frontend/src/components/common/Button.tsx
@@ -1,8 +1,10 @@
-import React, { HTMLAttributes } from 'react';
-import palette from '@styles/palette';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { css } from '@emotion/react';
+import palette from '@styles/palette';
 
 interface Props {
+  to?: string;
   fullWidth?: boolean;
   cyan?: boolean;
   style?: React.CSSProperties;
@@ -11,6 +13,7 @@ interface Props {
 }
 
 const Button = ({
+  to,
   fullWidth = false,
   cyan = false,
   style,
@@ -18,33 +21,36 @@ const Button = ({
   onClick,
   ...rest
 }: Props) => {
-  const handleClick = () => {
-    if (onClick) {
-      onClick();
+  const _style = css`
+    width: ${fullWidth ? '100%' : 'auto'};
+    padding: ${fullWidth ? '0.75rem 0' : '0.25rem 1rem'};
+    color: white;
+    font-size: ${fullWidth ? '1.125rem' : '1rem'};
+    font-weight: bold;
+    background: ${cyan ? palette.cyan[5] : palette.gray[8]};
+    border: 0;
+    border-radius: 0.25rem;
+    outline: 0;
+    cursor: pointer;
+    &:hover {
+      background: ${cyan ? palette.cyan[4] : palette.gray[6]};
     }
+  `;
+
+  const handleClick = () => {
+    onClick?.();
   };
 
+  if (to) {
+    return (
+      <Link to={to} css={_style}>
+        {children}
+      </Link>
+    );
+  }
+
   return (
-    <button
-      css={css`
-        width: ${fullWidth ? '100%' : 'auto'};
-        padding: ${fullWidth ? '0.75rem 0' : '0.25rem 1rem'};
-        color: white;
-        font-size: ${fullWidth ? '1.125rem' : '1rem'};
-        font-weight: bold;
-        background: ${cyan ? palette.cyan[5] : palette.gray[8]};
-        border: 0;
-        border-radius: 0.25rem;
-        outline: 0;
-        cursor: pointer;
-        &:hover {
-          background: ${cyan ? palette.cyan[4] : palette.gray[6]};
-        }
-      `}
-      style={style}
-      onClick={handleClick}
-      {...rest}
-    >
+    <button css={_style} style={style} onClick={handleClick} {...rest}>
       {children}
     </button>
   );
