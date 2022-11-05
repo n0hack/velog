8c4f0a178de5e8aa91d83524c06c7c4b96cb195e
n0hack <nohack-@naver.com>
2022-11-05 16:01:48 +0900

    [feat] 백엔드에서 정적 파일 배포 (koa-static)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,9 @@ import { koaSwagger } from 'koa2-swagger-ui';
 import yamljs from 'yamljs';
 import api from './api';
 import jwtMiddleware from '@lib/jwtMiddleware';
+import path from 'path';
+import serve from 'koa-static';
+import send from 'koa-send';
 
 dotenv.config();
 
@@ -30,9 +33,17 @@ const spec = yamljs.load('./api.yaml');
 router.get('/api', koaSwagger({ routePrefix: false, swaggerOptions: { spec } }));
 router.use('/api', api.routes());
 
-app.use(bodyParser());
+app.use(bodyParser({ jsonLimit: '50mb', formLimit: '50mb' }));
 app.use(jwtMiddleware);
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(__dirname, '../../frontend/build');
+app.use(serve(buildDirectory));
+app.use(async (ctx) => {
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    await send(ctx, 'index.html', { root: buildDirectory });
+  }
+});
 
 const port = PORT || 4000;
 app.listen(port, () => {
